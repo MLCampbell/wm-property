8f66d3b16c4e55849926675e88025e7fce013dc3
Marcus Campbell <marcus.l.campbell@gmail.com>
2022-10-16 18:36:46 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    fontFamily: { 'sans': ['Poppins', 'Roboto', 'sans-serif'] },
+    extend: {},
+  },
+  plugins: [],
+}
