e446aee9ab709cdb7afe18f191d4bea7c6ac06f4
Marcus Campbell <marcus.l.campbell@gmail.com>
2022-11-01 18:56:45 +0000

    responsive optimisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./src/index.html"],
   theme: {
+    'screens': {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1150px',
+      'xl': '1440px'
+    },
     fontFamily: { 'sans': ['Poppins', 'Roboto', 'sans-serif'] },
     extend: {
       colors: {
