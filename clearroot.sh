5ee0716f91616166dcb837231cb3116d40305d09
Marcus Campbell <marcus.l.campbell@gmail.com>
2022-11-10 21:24:28 +0000

    add favicons and adjust nav styling

--- clearroot.sh
+++ clearroot.sh
@@ -1,2 +1,2 @@
 shopt -s extglob;
-rm !(movetoroot.sh|clearroot.sh|.gitignore|.postcssrc|package-lock.json|package.json|tailwind.config.js)
+rm !(movetoroot.sh|clearroot.sh|.gitignore|.postcssrc|package-lock.json|package.json|tailwind.config.js|robots.txt|sitemap.xml)
