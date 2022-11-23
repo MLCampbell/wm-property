1677b24b613f21c20ad4966e49c5a216eecf77fc
Marcus Campbell <marcus.l.campbell@gmail.com>
2022-11-23 16:37:24 +0000

    remove fbq manual events for contact and lead

--- clearroot.sh
+++ clearroot.sh
@@ -1,2 +1,2 @@
 shopt -s extglob;
-rm !(movetoroot.sh|clearroot.sh|.gitignore|.postcssrc|package-lock.json|package.json|tailwind.config.js|robots.txt|sitemap.xml)
+rm !(movetoroot.sh|clearroot.sh|.gitignore|.postcssrc|package-lock.json|package.json|tailwind.config.js|robots.txt|sitemap.xml|README.md)
